44ab08a0d275e162c33f8d6c801848e65b82fbe6
fmshot <68507245+fmshot2@users.noreply.github.com>
2026-05-27 11:21:52 +0100

    Added debug error

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/debug-error', function () {
+    return response()->json([
+        'app_key_set' => !empty(config('app.key')),
+        'db_connection' => config('database.default'),
+        'db_url_set' => !empty(env('DB_URL')),
+        'app_env' => config('app.env'),
+        'storage_writable' => is_writable(storage_path()),
+    ]);
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
